22681c29805d6caf6aeb2a14498f21cc00b2993e
Tadeo Barranco <tbarrancoa@gmail.com>
2020-07-14 08:06:12 -0500

    Required files to create a new storefront theme

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Register the theme: 1' 16"
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Barranco/blank',
+    __DIR__
+);
